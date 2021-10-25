942c10d63bf486dd1b7cbfb0bc88c47f5e3bd294
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-10-25 22:35:04 +0700

    ⚡ 3rd commit

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,24 +2,44 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
   update(data) {
-    if (!data || (Array.isArray(data) && data.length === 0))
-      return this.renderError();
-
     this._data = data;
     const newMarkup = this._generateMarkup();
 
     const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = [...newDOM.querySelectorAll('*')];
+    const curElements = [...this._parentElement.querySelectorAll('*')];
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // Update changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('💥💥', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Update changed ATTRIBUTE
+      if (!newEl.isEqualNode(curEl))
+        [...newEl.attributes].forEach(attr => {
+          curEl.setAttribute(attr.name, attr.value);
+        });
+    });
   }
 
   _clear() {
