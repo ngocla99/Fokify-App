8c7c9beab69693ebe9f6769cc1301437e54fece3
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-10-27 21:46:14 +0700

    ⚡ 3rd commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '7726e983-4bc3-49a2-b9fa-db87f594ff99';
+export const MODAL_CLOSE_SEC = 2.5;
